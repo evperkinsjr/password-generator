e2a95b75635ebcb64ee9ee9c6a45995250e5ad31
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-05-12 23:06:30 -0400

    created arrays for the password characters and prompt for password length.

--- script.js
+++ script.js
@@ -14,10 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // need multiple arrays for computer to pick from
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numericalChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 // user clicks button to generate password
 
 // user is prompted to choose a password length 8-128 characters (type in a number)
+var pwLength = Number(window.prompt("Type a number between 8 and 128, please.", ""));
 
 // validate prompt
 
