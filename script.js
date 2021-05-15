27cd7252edad07acc644189346c045b3fb9e0379
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-05-15 15:33:30 -0400

    simplified approach after feedback from study group and teaching assistants.

--- script.js
+++ script.js
@@ -1,78 +1,69 @@
-// Assignment Code
+/* assignment code */
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+// String variables for computer to pick from
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!@#$%^&?_-+=";
+var selectedCharacters = "";
 
-    passwordText.value = password;
+// user clicks "Generate" button
+generateBtn.addEventListener("click", generate);
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// need multiple arrays for computer to pick from
-var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var numericalChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var specialChar = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
-var characterArray = [];
-// user clicks button to generate password
-function generatePassword() {
-    // user is prompted to choose a password length 8-128 characters (type in a number)
-    var pwLength = Number(window.prompt("Type a number between 8 and 128, please.", ""));
-
-    while (pwLength < 8 || pwLength > 128 || !pwLength || pwLength === NaN) {
+function generate() {
+	// Password Length Prompt
+	var pwLength = prompt("How many characters? Type a number between 8 and 128.");
+		if (pwLength >= 8 && pwLength <= 128) {
+			alert("Your password will be " + pwLength + " characters long.");
+		} else {
         alert("Please type a number between 8 and 128.");
-        Number(window.prompt("Type a number between 8 and 128, please.", ""));
-        if (pwLength >= 8 && pwLength <= 128) {
-            break;
-        }
+        prompt("Type a number between 8 and 128, please.");
     }
 
-    // prompts user for character types
-    var lowerChoice = confirm("Do you want lowercase letters?");
-    var upperChoice = confirm("Do you want uppercase letters?");
-    var numericalChoice = confirm("Do you want numbers?");
-    var specialChoice = confirm("Do you want special characters?");
+	// Character type prompts
+	var lowerCase = confirm("Do you want lowercase letters?")
+	var upperCase = confirm("Do you want uppercase letters?")
+	var numbers = confirm("Do you want numbers?")
+	var specChar = confirm("Do you want special characters?")
 
-    // checks if at least one character type was selected
-    while (!lowerChoice && !upperChoice && !numericalChoice && !specialChoice) {
+	 // Checks if at least one character type was selected
+    while (!lowerCase && !upperCase && !numbers && !specChar) {
         alert("Please choose at least one character type");
-        lowerChoice = confirm("Do you want lowercase letters?");
-        upperChoice = confirm("Do you want uppercase letters?");
-        numericalChoice = confirm("Do you want numbers?");
-        specialChoice = confirm("Do you want special characters?");
+        lowerCase = confirm("Do you want lowercase letters?");
+        upperCase = confirm("Do you want uppercase letters?");
+        numbers = confirm("Do you want numbers?");
+        specChar = confirm("Do you want special characters?");
     }
 
-    // if character types were selected, they're added to the password array.
-    if (lowerChoice) {
-        characterArray.push(lowerCaseChar);
-    }
-
-    if (upperChoice) {
-        characterArray.push(upperCaseChar);
-    }
-
-    if (numericalChoice) {
-        characterArray.push(numericalChar);
-    }
-
-    if (specialChoice) {
-        characterArray.push(specialChar);
-    }
+	// If character type selected, add it to password set
+	if (upperCase) {
+		selectedCharacters = selectedCharacters.concat(upperChar)
+	}
+	if (lowerCase) {
+		selectedCharacters = selectedCharacters.concat(lowerChar)
+	}
+	if (numbers) {
+		selectedCharacters = selectedCharacters.concat(numericalChar)
+	}
+	if (specChar) {
+		selectedCharacters = selectedCharacters.concat(specialChar)
+	}
+		generatePassword(pwLength);
+	
 }
 
+// Creating Password based on above selections
+function generatePassword(length) {
+	var result = "";
+	var characters = selectedCharacters;
+	var charactersLength = characters.length;
 
-// check if at least one character type is selected
-// if not, alert user that at least one character type must be selected then rerun character prompts AND repeat check
-
-// if at least one character type is selected THEN create password from selected criteria
-
-// display password in alert OR write in the textarea
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random()*charactersLength));
+	}
+	var passwordText = document.querySelector("#password");
+	passwordText.textContent = result
+	return;
+}
